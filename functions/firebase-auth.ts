dd227d8d983a8f381a018958c58213a988d3127d
Morgan Dean <morgandeanhi@gmail.com>
2023-02-17 21:44:00 -0800

    fixes

---
+++ functions/firebase-auth.ts
@@ -0,0 +1,14 @@
+export default {
+  type: 'service_account',
+  project_id: 'friendsfm',
+  private_key_id: '768247edefa6ae45ccd8cabd23f89fe3740c46fc',
+  private_key:
+    '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCUSWMPoUq+zh/Z\nlkYQwtQqS7IGUXaFjNVhkUXbUfikRno3oggvPbRdplswRwuz/K8GgiVeMuTZycUG\n90CkvoPzA5VJmwak0yPyjJdokkpK4iifKl/lPK2GnXiOpW5CCrm8vBVUMzs/jhO/\ne01H7Jni2m8UlImtdj/xKxuGjRhBNeNUv710pQzxVhTlW17oeiwpnKcj1Y3ha6Zf\n9phjbhHAu/i7lIuz9lYbgFBmGeE7zhFmTDHDxGBLu4yLH8JF5dCIwG/5qobvd/Q8\nPuNe+Ozrgh3mORcGkKGs11u9mHIgx1na1SbORqe5MbysJygvpL741kt33XHSKALD\n/9CVGEyfAgMBAAECggEAOvgMKtULcmFqDQ9TgarY0JEVlKmkpmthF9G7pAIvDLTy\nOVl4Q8EYDv3u2X4HS2Y6/C2ZSqKWm30/mwuMa3dP5b4RChpaRAWrD/OU6oLyhGD3\nooQpanH818Gbha6H8D9vqXxqRvewog+QDt+r1eRjNfGDpv2C2rZhqzhnU9ScRqHa\nT1lmiszcy3q8OorAOTC4qQORX2rmvFUuwJ/JT8Ju3uFLRy+KdlDwCq9Qm6L9p19z\nW6d4W/BGOtlAPcIm9LLDCb7mLCWnJZ5/QKp4DEqYLOtGMnWpjLAUDb1YzGte4jsU\nstsT0T3C/Rpw4KiQpW/kg7TbtQ4phRbLzT94IljaoQKBgQDLgHj05tRPFya8bJ2m\nH4HdEht3v7jmCl4dE0Bl/o1RB1hG9HIMP76rxdSO3nMQK5W5YOMxx7sJPn0KlDPx\n3kEhD97yeqKNORvduWA4Fi3JTmwJIMyFmfLhuzP051BD4O+igVe+vYfx7fmhJMow\nGhS0SGXo0c/vFdOQazFKp5JT5wKBgQC6im6bF6dfS5pRaAhoBeuoo2/fqPx5kBgi\nW/2WznSFEVImRU876g6LPVYl46K3vJ4ouUGXTLbT1CPRhMv7PkXR8erIiBbkreBa\nLauQcPFJOEVqbZIrXNg1F3EpuOkLZvoMHs+iw06DPNCYhaPMPExYDpYSM4ppLyyT\nyHWEAR6qiQKBgQCHDh44OjFQxAabKmJ9648eDnRYRs8JLHxFQ4+LoSJcUkmN3ZEK\n/AsoqMRmhM9MPgDB5MEjOnCws938dVr7/AqxTH0j46uZmfsabXqj/mQoo3kjnbbe\nO8trb/gNYq5sPg4LhVecaOCd+Neg0G+2+Lgg5aNsnkInZA7iW6F88DU2pQKBgDSu\n1hANPSLnyjyFgiJG0ch96CGlqzB15qsFifgmdLgELUxLtNX6A4OFnifzQ0hbB9v+\nfizJ6LSorDkHJUjezH8/sIxUCwsopD0zusl/bFW+g7RMH8UJYrKT3UzoQUEMI0B7\nDy8rs6fS7CK3U4xh1znjXLTlQ4jzXsaaDb5rRorpAoGANs9/FJSV+Z/3cWWXmreR\nU9QLi898rFpoIITkgJnerFw/zDjWJ1dWVmfOW9loUQrt+q0uyahHf9pUehrvShMp\nFEfGi7x/Aw1CQtZ5gCukcNpmegTbc3FMKLFSiy6oT6+LjmYk/4o5XcqmKMOwQj2P\nRl1dN5AmW/9UVLvi35EY2NI=\n-----END PRIVATE KEY-----\n',
+  client_email: 'firebase-adminsdk-d4y0g@friendsfm.iam.gserviceaccount.com',
+  client_id: '104171217347166817887',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-d4y0g%40friendsfm.iam.gserviceaccount.com',
+};
