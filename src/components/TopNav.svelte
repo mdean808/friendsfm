859c90a7ff6395f787984065235633b45ef2d80a
Morgan Dean <morgandeanhi@gmail.com>
2023-03-07 10:18:47 -0800

    add friend request indicator to friends icon

--- src/components/TopNav.svelte
+++ src/components/TopNav.svelte
@@ -15,7 +15,7 @@
       $user?.musicPlatform
     )}`}
   >
-    <button class="flex-grow-0" on:click={() => goto('/friends')}>
+    <button class="flex-grow-0 relative" on:click={() => goto('/friends')}>
       <svg
         class="w-8 h-8"
         fill="currentColor"
@@ -25,6 +25,15 @@
           d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"
         /></svg
       >
+      {#if $user.friendRequests.length > 0}
+        <div
+          class="absolute inline-flex items-center justify-center w-4 h-4 text-xs font-bold text-white bg-red-500 rounded-full top-0 -right-2"
+        >
+          {$user.friendRequests.length > 9
+            ? $user.friendRequests.length + '+'
+            : $user.friendRequests.length}
+        </div>
+      {/if}
     </button>
     <h1 class="text-center mx-auto text-2xl text-white truncate flex-grow px-4">
       {$user.username}
