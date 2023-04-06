a9db76f1bbda089afb1ec716420ba2912a863b29
Morgan Dean <morgandeanhi@gmail.com>
2023-04-06 11:28:55 -0700

    update version, add source map generation for sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,27 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    sentryVitePlugin({
+      org: 'friendsfm',
+      project: 'friendsfm-app',
+
+      // Specify the directory containing build artifacts
+      include: './dist',
+
+      // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+      // and need `project:releases` and `org:read` scopes
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+
+      // Optionally uncomment the line below to override automatic release name detection
+      release: 'friendsfm@' + process.env.RELEASE,
+      telemetry: false,
+    }),
+  ],
   server: {
     port: 8080,
     host: '0.0.0.0',
