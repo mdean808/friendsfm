42b0f21733f47b3eb966d91ffa69ec4cd4900da3
Morgan Dean <morgandeanhi@gmail.com>
2023-11-21 16:17:19 -0800

    fix delte songe

--- functions/src/endpoints/songs/deletesong.ts
+++ functions/src/endpoints/songs/deletesong.ts
@@ -2,13 +2,12 @@ import { SavedSong } from '@/types';
 import { onRequest } from 'firebase-functions/v2/https';
 import { authMiddleware, sentryWrapper } from '../middleware';
 
-//todo: THIS ISN'T WORKING
 export const deletesong = onRequest(
   { cors: true },
   authMiddleware(
     sentryWrapper('delete-song', async (req, res, user) => {
       const { song }: { song: SavedSong } = JSON.parse(req.body);
-      user.unsaveSong(song);
+      await user.unsaveSong(song);
       res.status(200).type('json').send({ type: 'success', message: '' });
     })
   )
