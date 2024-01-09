bed671bba30be9f2875cb6cdf2e77c5a8a7406fc
Morgan Dean <morgandeanhi@gmail.com>
2024-01-09 07:25:05 -0800

    update og tags

--- web/src/routes/user/[username]/+page.svelte
+++ web/src/routes/user/[username]/+page.svelte
@@ -22,6 +22,16 @@
   };
 </script>
 
+<svelte:head>
+  <meta property="og:title" content={`${username} | FriendsFM`} />
+  <meta property="og:site_name" content="FriendsFM" />
+  <meta property="og:url" content={`https://friendsfm.app/user/{username}`} />
+  <meta
+    property="og:description"
+    content={`listen with ${username} on FriendsFM`}
+  />
+</svelte:head>
+
 <div class="w-full mx-auto p-2">
   <div class="text-white mx-auto text-center py-2">
     <div class="w-full mx-auto pb-1 border-b-2 border-white">
